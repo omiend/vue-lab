a2e1ee8bdc2527c56e6206f752e8d9326677c01b
omiend <omiend@gmail.com>
2019-06-23 16:03:44 +0900

    Lab 2 - 基本的なレンダリング - Exercise 2: リストレンダリング

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,23 +2,45 @@
   <div class="home">
     <h3>テキストバインディング</h3>
     <span>{{ msg }}</span>
+    <h3>リストレンダリング</h3>
+    <ul id="item-list">
+      <li v-for="item in items" :key="item.id">
+        {{item.name}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
+import Vue from "vue"
+
+// ここにインターフェースを追加
+declare interface Item {
+  id: number;
+  name: string;
+}
 
 export default Vue.extend({
   data() {
     return {
-      msg: "Hello Vue.js!"
-    };
+      msg: "Hello Vue.js!",
+      items: [
+        {
+          id: 1,
+          name: "キーボード"
+        },
+        {
+          id: 2,
+          name: "マウス"
+        }
+      ] as Item[]
+    }
   }
-});
+})
 </script>
 
 <style scoped>
 .home {
-  text-align: left;
+  text-align: left
 }
 </style>
